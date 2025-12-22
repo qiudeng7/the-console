12ce26440fff86e339d15b5e9e0f77bd52f34bc2
qiudeng7 <2143975743@qq.com>
2025-12-22 15:41:58 +0800

    配置alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 import { cloudflare } from "@cloudflare/vite-plugin"
+
+function createAlias(aliasList: Record<string, string>) {
+	const result: Record<string, string> = {}
+	for (const [key, value] of Object.entries(aliasList)) {
+		result[key] = fileURLToPath(new URL(value, import.meta.url))
+	}
+	return result
+}
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,7 +21,11 @@ export default defineConfig({
 	],
 	resolve: {
 		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url))
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			...createAlias({
+				"#server/*": "./server/*",
+				"#drizzle-client": "./server/utils/drizzle/client"
+			})
 		},
 	},
 })
